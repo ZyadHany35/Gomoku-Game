9a7455dae26b7f6015fc5a2cc1535ba0fa838a18
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2025-05-14 15:54:54 +0300

    add turn label

--- GumokuBoard.py
+++ GumokuBoard.py
@@ -11,10 +11,11 @@ class GomokuBoard:
     def __init__(self, root):
         self.root = root
         self.root.title("Gomoku Game")
-
+        # title
         label = tk.Label(root, text="Gomoku AI Game",font=("Helvetica", 20, "bold"))
         label.pack(padx=10)
 
+        # board
         self.canvas = tk.Canvas(root, width=BOARD_SIZE * CELL_SIZE, height=BOARD_SIZE * CELL_SIZE + 20,background="#d2a77c")
         self.canvas.pack()
         self.canvas.bind("<Button-1>", self.human_move)
@@ -24,13 +25,26 @@ class GomokuBoard:
         self.human_turn = True
         self.ai_vs_ai_mode = False
 
-
         self.draw_board()
 
+        # turns
+        self.turn_frame = tk.Frame(root, borderwidth=2, relief="groove", pady=5)
+        self.turn_frame.pack(pady=(5, 0))
+
+        self.turn_label = tk.Label(
+            self.turn_frame,
+            text="Turn: Human",
+            font=("Helvetica", 14, "bold"),
+            fg="black"
+        )
+        self.turn_label.pack()
+
+
+        # dropout 
         self.algorithm_var = tk.IntVar(value=(1,"Minimax"))
         self.create_algorithm_selector()
         
-
+        # buttons
         self.bottom_frame = tk.Frame(root, padx=10, pady=10)
         self.bottom_frame.pack()
 
@@ -52,7 +66,7 @@ class GomokuBoard:
 
         self.ai_btn = HoverButton(
             self.bottom_frame,
-            text="AI vs AI",
+            text="MiniMax vs Alphabeta",
             command=self.ai_vs_ai,
             bg="#7CA7D2",
             fg="black",
@@ -69,6 +83,17 @@ class GomokuBoard:
         self.canvas.bind("<Motion>", self.hover_highlight)
         self.highlight_id = None
 
+    def update_turn_label(self):
+        if self.ai_vs_ai_mode:
+            algo = self.algorithm_var.get()
+            ai_player = "Minimax" if self.game.current_player == 1 else "AlphaBeta"
+            self.turn_label.config(text=f"Turn: {ai_player}")
+        else:
+            if self.human_turn:
+                self.turn_label.config(text="Turn: Human")
+            else:
+                algo = self.algorithm_var.get()
+                self.turn_label.config(text=f"Turn: {algo}")
 
     def draw_board(self):
         self.canvas.delete("all")
@@ -94,17 +119,19 @@ class GomokuBoard:
                     )
 
         self.canvas.create_line(
-        0, BOARD_SIZE * CELL_SIZE + 20,
-        BOARD_SIZE * CELL_SIZE, BOARD_SIZE * CELL_SIZE + 20,
-        fill="black",
-        width=4)
+            0, BOARD_SIZE * CELL_SIZE + 20,
+            BOARD_SIZE * CELL_SIZE, BOARD_SIZE * CELL_SIZE + 20,
+            fill="black",
+            width=4)
+
+
 
     def create_algorithm_selector(self):
         selector_frame = tk.Frame(self.root)
         selector_frame.pack(pady=10)
 
         self.algorithm_var = tk.StringVar(value="Minimax")
-        tk.Label(selector_frame, text="AI Algorithm:").pack(side=tk.LEFT, padx=5)
+        tk.Label(selector_frame, text="AI Algorithm:", font=("Helvetica", 10, "bold")).pack(side=tk.LEFT, padx=5)
 
         algo_menu = tk.OptionMenu(
             selector_frame,
@@ -121,7 +148,7 @@ class GomokuBoard:
         )
 
         self.difficulty_var = tk.StringVar(value="Easy")
-        tk.Label(selector_frame, text="Difficulty:").pack(side=tk.LEFT, padx=5)
+        tk.Label(selector_frame, text="Difficulty:", font=("Helvetica", 10, "bold")).pack(side=tk.LEFT, padx=5)
 
         diff_menu = tk.OptionMenu(
             selector_frame,
@@ -168,10 +195,11 @@ class GomokuBoard:
 
             self.draw_board()
             if self.game.check_win():
-                messagebox.showinfo("Game Over", f"{'Black' if self.game.current_player == 2 else 'White'} wins!")
+                messagebox.showinfo("Game Over", f"{'Black (human)' if self.game.current_player == 2 else 'White (ai)'} wins!")
                 self.human_turn = False
                 return
             self.human_turn = False
+            self.update_turn_label()
             self.root.after(500, self.ai_move)
 
     def ai_move(self):
@@ -185,22 +213,23 @@ class GomokuBoard:
             self.game.make_move(*move)
             self.draw_board()
             if self.game.check_win():
-                winner = 'Black' if self.game.current_player == 2 else 'White'
-                messagebox.showinfo("Game Over", f"{winner} wins!")
+                messagebox.showinfo("Game Over", f"{'Black (human)' if self.game.current_player == 2 else 'White (ai)'} wins!")
                 return
         self.human_turn = True
+        self.update_turn_label()
 
 
     def reset_game(self):
         self.game.reset()
         self.human_turn = True
         self.ai_vs_ai_mode = False
+        self.update_turn_label()
         self.draw_board()
 
     def ai_vs_ai(self):
         self.ai_vs_ai_mode = True
         if self.game.check_win():
-            messagebox.showinfo("Game Over", f"{'Black' if self.game.current_player == 2 else 'White'} wins!")
+            messagebox.showinfo("Game Over", f"{'Black (minimax)' if self.game.current_player == 2 else 'White (alphabeta)'} wins!")
             return
 
         depth = 1 if self.difficulty_var.get() == "Easy" else 2
@@ -214,5 +243,6 @@ class GomokuBoard:
             self.game.make_move(*move)
             self.draw_board()
 
+        self.update_turn_label()
         self.root.after(500, self.ai_vs_ai)
 
